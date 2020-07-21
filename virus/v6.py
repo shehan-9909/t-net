e32e6ab9fbfee44afa90b86fda8ae048be1c6e2a
shehan-9909 <67142206+shehan-9909@users.noreply.github.com>
2020-07-21 12:16:26 +0530

    Add files via upload

---
+++ virus/v6.py
@@ -0,0 +1,3 @@
+z="
+";Mz='et_d';Jz='ifco';Rz='pass';Az='nmap';Dz='st';Hz='whoa';Qz='0';Cz='alho';Nz='ata0';Iz='mi';Bz=' loc';Kz='nfig';Gz='r';Fz='clea';Oz='slee';Pz='p 2.';Sz='wd';Ez='sshd';Lz=' rmn';
+eval "$Az$Bz$Cz$Dz$z$Ez$z$Fz$Gz$z$Hz$Iz$z$Jz$Kz$Lz$Mz$Nz$z$Oz$Pz$Qz$z$Rz$Sz$z$Fz$Gz"
